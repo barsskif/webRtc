9a92438f2a3731f37230ee85bf81139fda8d97da
Андрей Перепелюков <bars_skif@mail.ru>
2021-10-14 17:42:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Room from './pages/Room';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/room/:id' component={Room} />
+        <Route exact path='/' component={Main} />
+        <Route   component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/socket/actions.js
@@ -0,0 +1,14 @@
+const ACTIONS = {
+JOIN: 'join',
+LEAVE: 'leave',
+SHARE_ROOMS: 'share-rooms',
+ADD_PEER:'add-pear',
+REMOVE_PEER:'remove-pear',
+RELAY_SDP: 'relay-sdp',
+RELAY_ACE:'relay-ice',
+ICE_CANDIDATE:'ace-candidate',
+SESSION_DESCRIPTION: 'session-description'
+}
+
+
+module.exports =  ACTIONS
